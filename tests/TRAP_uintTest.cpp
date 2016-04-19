de3fff9c299e306ca5a9b83660edcbadd09888bd
Michael Bartling <michael.bartling15+dev@gmail.com>
2016-04-19 11:38:40 -0500

    Updated trap unittest to fit

--- tests/TRAP_uintTest.cpp
+++ tests/TRAP_uintTest.cpp
@@ -16,11 +16,11 @@ TEST(CheckTRAP, All) {
         sim3.setPC(0x3000 - 1);
         sim3.doInst(TRAP | SETDR(1) | 0x2);
         EXPECT_EQ(sim3.getReg(7), 0x3000);
-        EXPECT_EQ(sim3.getPC(), 0x0002);
+        EXPECT_EQ(sim3.getPC(), sim3.memory[0x0002]);
 
         sim3.setPC(0x4000 - 1);
         sim3.doInst(TRAP | SETDR(1) | 0x20);
         EXPECT_EQ(sim3.getReg(7), 0x4000);
-        EXPECT_EQ(sim3.getPC(), 0x0020);
+        EXPECT_EQ(sim3.getPC(), sim3.memory[0x0020]);
 
 }
