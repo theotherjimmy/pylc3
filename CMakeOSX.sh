e4316d008f52611e3c3248717c34e20fbe57b919
Michael Bartling <michael.bartling15@gmail.com>
2016-09-13 20:28:01 -0500

    Updated pylc3 to actually build multiple targets

--- CMakeOSX.sh
+++ CMakeOSX.sh
@@ -1,4 +1,5 @@
 mkdir -p build
 cd build
-cmake -DPREFIX=build -DPYTHON_LIBRARY=/usr/local/Cellar/python/2.7.12/Frameworks/Python.framework/Versions/2.7/lib/libpython2.7.dylib ..
+#cmake -DPREFIX=build -DPYTHON_LIBRARY=/usr/local/Cellar/python/2.7.12/Frameworks/Python.framework/Versions/2.7/lib/libpython2.7.dylib ..
+cmake -DCMAKE_PREFIX_PATH="/usr/local/Cellar/qt5/5.6.1-1/;/usr/local/Cellar/python/2.7.12" -DPYTHON_LIBRARY=/usr/local/Cellar/python/2.7.12/Frameworks/Python.framework/Versions/2.7/lib/libpython2.7.dylib ..
 make
